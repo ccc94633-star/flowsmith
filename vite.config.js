1943e2a1a1e44956a63f5bd94c30f4de6c8cbde9
ccc94633-star <ccc94633@gmail.com>
2026-06-25 14:00:56 +0800

    修正base位置

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: '/flowsmith/',
   plugins: [vue()],
   server: {
     open: true,
-    base: '/flowsmith/',
   }
 });
